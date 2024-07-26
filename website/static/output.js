6a3c090e7752f4cfd7e87b28d932b6f1baedbb1f
Adel Sevastianov <adelsevastanov@gmail.com>
2024-07-26 11:46:08 +0300

    add: button for copy short url

---
+++ website/static/output.js
@@ -0,0 +1,26 @@
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const shortenedUrl = getQueryParam('shortenedUrl');
+    const shortUrlField = document.getElementById('shortUrl');
+
+    if (shortenedUrl) {
+        shortUrlField.value = shortenedUrl;
+    } else {
+        shortUrlField.value = 'No URL provided.';
+    }
+
+    document.getElementById('copyButton').addEventListener('click', function () {
+        shortUrlField.select();
+        shortUrlField.setSelectionRange(0, 99999);
+
+        navigator.clipboard.writeText(shortUrlField.value).then(function () {
+            console.log('URL copied to clipboard');
+        }).catch(function (error) {
+            console.error('Failed to copy URL: ', error);
+        });
+    });
+});
