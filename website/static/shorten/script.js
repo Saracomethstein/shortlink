92a62bef8d26ab2f95c08c91b4cbabc92457f36b
barathea <barathea@student.21-school.ru>
2024-08-03 14:15:43 +0300

    to-do: set cookie and styles for pages

--- website/static/shorten/script.js
+++ website/static/shorten/script.js
@@ -1,3 +1,42 @@
+document.getElementById('urlForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    const sessionId = getCookie('session_id');
+    const url = document.getElementById('urlInput').value;
+
+    if (!sessionId) {
+        alert('Please authorization.')
+        return;
+    }
+
+    if (!url) {
+        alert('Please enter a URL.');
+        return;
+    }
+
+    try {
+        const response = await fetch('/api/shorten', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ url })
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        document.cookie = `shortenedUrl=${encodeURIComponent(data.shortenedUrl)}; path=/; domain=${window.location.hostname}`;
+        console.log('Cookies set:', document.cookie);
+
+        window.location.href = '/output/';
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        alert('There was an error shortening the URL.');
+    }
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     const sessionId = getCookie('session_id');
     const authButton = document.querySelector('.auth-button');
@@ -39,40 +78,3 @@ function getCookie(name) {
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
-
-document.getElementById('urlForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
-    const sessionId = getCookie('session_id');
-    const url = document.getElementById('urlInput').value;
-
-    if (!sessionId) {
-        alert('Please authorization.')
-        return
-    }
-
-    if (!url) {
-        alert('Please enter a URL.');
-        return;
-    }
-
-    try {
-        const response = await fetch('/api/shorten', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ url })
-        });
-
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        document.cookie = `shortenedUrl=${encodeURIComponent(data.shortenedUrl)}; path=/`;
-        window.location.href = '/output/';
-    } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-        alert('There was an error shortening the UR L.');
-    }
-});
